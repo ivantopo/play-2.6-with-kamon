20de9e8929b86091af76a879c7be2e9d30e04724
Ivan Topolnjak <ivantopo@gmail.com>
2018-03-04 23:49:03 +0100

    add trace ID and request ID logging

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,15 +1,22 @@
 package controllers
 
 import javax.inject._
+
+import kamon.Kamon
+import kamon.context.Key
 import play.api._
 import play.api.mvc._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)(implicit ec: ExecutionContext) extends AbstractController(cc) {
+  val key = Key.broadcastString("request-id")
+  val logger = Logger("HomeController")
 
   /**
    * Create an Action to render an HTML page.
@@ -18,7 +25,19 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def index() = Action.async { implicit request: Request[AnyContent] =>
+    val requestID = Kamon.currentContext().get(key).getOrElse("no-request-id")
+    logger.info("RequestID in the action: " + requestID)
+
+    Future {
+      logger.info("Logging in the Future Body")
+
+    } map { _ =>
+      logger.info("Logging in the map")
+
+    } flatMap { _ =>
+      logger.info("Logging in the flatMap")
+      Future.successful(Ok(views.html.index()))
+    }
   }
 }
